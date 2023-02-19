16275a961d0eea6ed0ea3c5b2046314f485191c5
aryagirigoudar <aryagirigoudar05072002@gmail.com>
2023-02-19 11:36:49 +0530

    My collage stuff push no 1

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "That's me PES1UG21CS806";
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
